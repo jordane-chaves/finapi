cfa81daf6972c62b187d469ec86663172ec6b16b
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-07-27 19:10:12 -0300

    Should be able to create a new user

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -1,4 +1,7 @@
 import { Connection } from 'typeorm';
+import request from 'supertest';
+
+import { app } from '../../../../app';
 import createConnection from '../../../../database';
 
 let connection: Connection;
@@ -9,7 +12,19 @@ describe('CreateUserController', () => {
     await connection.runMigrations();
   });
 
-  it('test', () => {
-    expect(1 + 1).toBe(2);
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/api/v1/users')
+      .send({
+        name: "User supertest",
+        email: "user@test.com",
+        password: "test-password"
+      });
+
+    expect(response.status).toBe(201);
   });
 });
