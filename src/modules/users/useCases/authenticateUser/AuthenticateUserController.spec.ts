eff19ed9fc8e34964b80efc7440a01c8571ad279
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-07-27 19:27:35 -0300

    Should be able to authenticate user

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,37 @@
+import { Connection } from 'typeorm';
+import request from 'supertest';
+
+import { app } from '../../../../app';
+import createConnection from '../../../../database';
+
+let connection: Connection;
+
+describe('AuthenticateUserController', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to authenticate user', async () => {
+    const user = {
+      name: "UserToAuthenticate",
+      email: "user-auth@test.com",
+      password: "test-auth-password"
+    };
+
+    await request(app).post('/api/v1/users').send(user);
+
+    const response = await request(app).post('/api/v1/sessions').send({
+      email: user.email,
+      password: user.password
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+  });
+});
